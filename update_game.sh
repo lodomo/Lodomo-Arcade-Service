c47cd7068933fceef7b2106ae0bbe85f23788946
Lodomo <lodomo.dev@gmail.com>
2026-06-22 10:53:35 -0700

    Let's try this again

--- update_game.sh
+++ update_game.sh
@@ -7,10 +7,20 @@ fi
 # CD into the game folder
 cd game
 
-BASE=$(cat lodomo.location | tr -d '\n')
-curl "${BASE}/game.pck"
-curl "${BASE}/game.x86_64"
-curl "${BASE}/version.md"
+BASE=$(cat ../lodomo.location | tr -d '\r\n' | xargs)
+echo "Base URL for game files: ${BASE}"
+# Sending a curl to
+echo "Downloading game files from ${BASE}..."
+
+echo "Downloading game.pck from ${BASE}/game.pck..."
+curl -L -O "${BASE}/game.pck"
+
+echo "Downloading game.x86_64 from ${BASE}/game.x86_64..."
+curl -L -O "${BASE}/game.x86_64"
+
+echo "Downloading version.md from ${BASE}/version.md..."
+curl -L -O "${BASE}/version.md"
 
 # Restart lodomo-arcade.service
 sudo systemctl restart lodomo-arcade.service
+
